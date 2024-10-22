1959d6f58883328706c134d1883b9439495231f9
Michael3607 <mortemic004@alpinesd.org>
2024-10-22 12:10:08 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -2,8 +2,8 @@ userInput = int(input())
 sum = 0  
 loopedTimes = 0  
   
- while loopedTimes < userInput:  
+while loopedTimes < userInput:  
     loopedTimes += 1  
     sum += loopedTimes
   
- print(sum)
+print(sum)
