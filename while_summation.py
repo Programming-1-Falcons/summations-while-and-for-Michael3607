6e6f3e44259abe19bc6d23da320c9ec5089d18a1
Michael3607 <mortemic004@alpinesd.org>
2024-10-22 11:24:41 -0600

    Update while_summation.py

--- while_summation.py
+++ while_summation.py
@@ -1,6 +1,6 @@
 #while summation code following directions from Readme
 Num = int(input("give a number:")
-baseNum = 0
-while(Num > baseNum):
-  print(baseNum)
+baseNum = 1
+while(Num >= baseNum):
+  print(baseNum): print("+")
   baseNum + 1
